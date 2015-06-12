4f380054b9bd9218725b4720c9fd7c8b86e90d2e
ValoXTech <valoxblood@gmail.com>
2015-06-12 09:37:11 +0200

    Update

--- hazy_mako.mk
+++ hazy_mako.mk
@@ -2,7 +2,7 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common Hazy stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/hazy/configs/common_full_phone.mk)
 
 # Enhanced NFC
@@ -19,8 +19,8 @@ PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1/LMY47O/1783956:user/release-keys \
-   PRIVATE_BUILD_DESC="occam-user 5.1 LMY47O 1783956 release-keys"
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY47V/1836172:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY47V 1836172 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
